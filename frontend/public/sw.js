3e63f6a1a268885f27584b26d1fd9099595395e9
multigymsistema-debug <multigymsistema@gmail.com>
2026-09-03 00:56:14 -0300

    feat: add app install assets

---
+++ frontend/public/sw.js
@@ -0,0 +1,5 @@
+const CACHE='multigym-shell-v1';
+const SHELL=['/','/manifest.webmanifest','/icon.svg'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',event=>{event.waitUntil(self.clients.claim())});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET'||new URL(event.request.url).origin!==location.origin)return;event.respondWith(fetch(event.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));return r}).catch(()=>caches.match(event.request).then(r=>r||caches.match('/'))))});
